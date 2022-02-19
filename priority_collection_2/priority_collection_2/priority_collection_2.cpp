a7fc2c7a26fec3ec62a4782eae0323403d197553
ilyabryukhov <ilyaking1@gmail.com>
2022-02-19 18:41:24 +0300

    try

--- priority_collection_2/priority_collection_2/priority_collection_2.cpp
+++ priority_collection_2/priority_collection_2/priority_collection_2.cpp
@@ -1,20 +1,91 @@
-﻿// priority_collection_2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-
+﻿#include "D:/git/cppRepos/test_runner.h"
 #include <iostream>
+#include <iterator>
+#include <memory>
+#include <list>
+#include <utility>
+#include <vector>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+using namespace std;
+
+template <typename T>
+class PriorityCollection {
+    struct Node {
+        int d_priority;
+        T d_data;
+    };
+
+    list<Node> d_priorityList;
+
+public:
+    using Id = typename list<Node>::iterator;
+    Id Add(T object) {
+        Node newObject{ 0, move(object) };
+        d_priorityList.push_back(newObject);
+        return --d_priorityList.end();
+    }
+
+    template <typename ObjInputIt, typename IdOutputIt>
+    void Add(ObjInputIt range_begin, ObjInputIt range_end,
+        IdOutputIt ids_begin);
+
+
+    bool IsValid(Id id) const;
+
+    const T& Get(Id id) const;
+
+    void Promote(Id id);
+
+    pair<const T&, int> GetMax() const;
+    pair<T, int> PopMax();
+
+
+    
+};
+
+
+class StringNonCopyable : public string {
+public:
+    using string::string;  
+    StringNonCopyable(const StringNonCopyable&) = delete;
+    StringNonCopyable(StringNonCopyable&&) = default;
+    StringNonCopyable& operator=(const StringNonCopyable&) = delete;
+    StringNonCopyable& operator=(StringNonCopyable&&) = default;
+};
+
+void TestNoCopy() {
+    PriorityCollection<StringNonCopyable> strings;
+    const auto white_id = strings.Add("white");
+    const auto yellow_id = strings.Add("yellow");
+    const auto red_id = strings.Add("red");
+
+    strings.Promote(yellow_id);
+    for (int i = 0; i < 2; ++i) {
+        strings.Promote(red_id);
+    }
+    strings.Promote(yellow_id);
+    {
+        const auto item = strings.PopMax();
+        ASSERT_EQUAL(item.first, "red");
+        ASSERT_EQUAL(item.second, 2);
+    }
+    {
+        const auto item = strings.PopMax();
+        ASSERT_EQUAL(item.first, "yellow");
+        ASSERT_EQUAL(item.second, 2);
+    }
+    {
+        const auto item = strings.PopMax();
+        ASSERT_EQUAL(item.first, "white");
+        ASSERT_EQUAL(item.second, 0);
+    }
 }
 
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+int main() {
+    //TestRunner tr;
+    //RUN_TEST(tr, TestNoCopy);
+    PriorityCollection<int> pc;
+    auto ptr = pc.Add(2);
 
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
+    return 0;
+}

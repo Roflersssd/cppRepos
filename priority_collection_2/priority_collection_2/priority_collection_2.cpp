9f7b1a8b39153e70cecb610b79448c4af7dafa1a
ilyabryukhov <ilyaking1@gmail.com>
2022-02-20 18:08:20 +0300

    complete

--- priority_collection_2/priority_collection_2/priority_collection_2.cpp
+++ priority_collection_2/priority_collection_2/priority_collection_2.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 #include <iterator>
 #include <memory>
-#include <list>
+#include <set>
 #include <utility>
 #include <vector>
 
@@ -10,36 +10,65 @@ using namespace std;
 
 template <typename T>
 class PriorityCollection {
-    struct Node {
-        int d_priority;
-        T d_data;
-    };
-
-    list<Node> d_priorityList;
-
 public:
-    using Id = typename list<Node>::iterator;
+
+    using Id = typename int;
+
     Id Add(T object) {
-        Node newObject{ 0, move(object) };
-        d_priorityList.push_back(newObject);
-        return --d_priorityList.end();
+        d_objects.push_back({ move(object),0 });
+        Id id = d_objects.size() - 1;
+        d_pSet.insert({ 0,id });
+        return id;
     }
 
     template <typename ObjInputIt, typename IdOutputIt>
     void Add(ObjInputIt range_begin, ObjInputIt range_end,
-        IdOutputIt ids_begin);
+        IdOutputIt ids_begin) {
+        while (range_begin != range_end) {
+            *ids_begin++ = Add(move(*range_begin++));
+        }
+    }
 
 
-    bool IsValid(Id id) const;
+    bool IsValid(Id id) const {
+        return id < d_objects.size() && d_objects[id].d_piority != INVALID_PRIORITY;
+    }
 
-    const T& Get(Id id) const;
+    const T& Get(Id id) const {
+        return d_objects[id];
+    }
 
-    void Promote(Id id);
+    void Promote(Id id) {
+        auto temp = d_pSet.extract({ d_objects[id].d_piority, id });
+        d_objects[id].d_piority++;
+        temp.value().first = d_objects[id].d_piority;
+        d_pSet.insert(move(temp));
+    }
 
-    pair<const T&, int> GetMax() const;
-    pair<T, int> PopMax();
+    pair<const T&, int> GetMax() const {
+        auto ptr = d_pSet.crbegin();
+        Id id = ptr->second;
+        return { d_objects[id].d_data, d_objects[id].d_piority };
+    }
 
+    pair<T,int> PopMax() {
+        auto ptr = prev(d_pSet.end());
+        Id id = ptr->second;
+        d_pSet.erase(ptr);
+        pair<T, int> maxValue = { move(d_objects[id].d_data), d_objects[id].d_piority };
+        d_objects[id].d_piority = INVALID_PRIORITY;
+        return maxValue;
+    }
+private:
+    struct Object {
+        T d_data;
+        int d_piority;
 
+    };
+
+    set<pair<int, Id>> d_pSet;
+    vector<Object> d_objects;
+    const int INVALID_PRIORITY = -1;
     
 };
 
@@ -82,10 +111,7 @@ void TestNoCopy() {
 }
 
 int main() {
-    //TestRunner tr;
-    //RUN_TEST(tr, TestNoCopy);
-    PriorityCollection<int> pc;
-    auto ptr = pc.Add(2);
-
+    TestRunner tr;
+    RUN_TEST(tr, TestNoCopy);
     return 0;
 }
